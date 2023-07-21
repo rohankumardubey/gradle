57a6ad5b730c10253678c2699f2a715405388a38
Sergey Igushkin <sigushkin@gradle.com>
2023-07-21 15:02:06 +0400

    Simplify code around adding `InstrumentableClosure` interface to closures

--- subprojects/core/src/main/java/org/gradle/internal/classpath/CallInterceptionClosureInstrumentingClassVisitor.java
+++ subprojects/core/src/main/java/org/gradle/internal/classpath/CallInterceptionClosureInstrumentingClassVisitor.java
@@ -19,6 +19,7 @@ package org.gradle.internal.classpath;
 import groovy.lang.Closure;
 import org.gradle.api.NonNullApi;
 import org.gradle.model.internal.asm.MethodVisitorScope;
+import org.jetbrains.annotations.NotNull;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
@@ -230,11 +231,17 @@ public class CallInterceptionClosureInstrumentingClassVisitor extends ClassVisit
     public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
         boolean isClosureImplementation = CLOSURE_INTERNAL_NAME.equals(superName);
         enterClass(name, isClosureImplementation);
+        String[] modifiedInterfaces = interfacesWithInstrumentableClosure(interfaces, isClosureImplementation);
+        super.visit(version, access, name, signature, superName, modifiedInterfaces);
+    }
+
+    @NotNull
+    private static String[] interfacesWithInstrumentableClosure(String[] interfaces, boolean isClosureImplementation) {
         String[] modifiedInterfaces = isClosureImplementation ? Arrays.copyOf(interfaces, interfaces.length + 1) : interfaces;
         if (isClosureImplementation) {
             modifiedInterfaces[modifiedInterfaces.length - 1] = Type.getInternalName(InstrumentableClosure.class);
         }
-        super.visit(version, access, name, signature, superName, modifiedInterfaces);
+        return modifiedInterfaces;
     }
 
     @Override
